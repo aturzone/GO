8d536fdc0c2d4709c77df147abf24b04160a519a
aturzone <aturzone@gmail.com>
2025-07-27 16:26:20 +0330

    error handler

--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,7 @@
 module github.com/aturzone/go/hello
 
 go 1.24.5
+
+require github.com/aturzone/go/greetings v0.0.0
+
+replace github.com/aturzone/go/greetings v0.0.0 => ../greetings
--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"github.com/aturzone/go/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Atur")
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greeting.Hello("Atur")
+	if err != nill {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+
+	message, err = greeting.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
